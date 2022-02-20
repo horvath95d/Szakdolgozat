2a3d976775549477143897ae368134239850b69f
Daniel Horvath <daniel.horvath@accretivetg.com>
2022-02-20 18:23:58 +0100

    Add test user 3

--- docker/mysql/03-insertData.sql
+++ docker/mysql/03-insertData.sql
@@ -5,13 +5,15 @@ START TRANSACTION;
 SET NAMES utf8mb4;
 
 INSERT INTO `school` (`id`, `owner_id`, `full_name`, `short_name`, `year`, `days`, `emblem`, `code`) VALUES
-    (1, 1, 'Roxfort Boszorkány- és Varázslóképző Szakiskola', 'Roxfort', '2021/2022', 5, 'roxfort.png', 'phoenix');
+    (1, 1, 'Roxfort Boszorkány- és Varázslóképző Szakiskola', 'Roxfort', '2021/2022', 5, 'roxfort.png', 'phoenix'),
+    (2, 3, 'Egy teljesen átlagos iskola', 'Iskola', '2021/2022', 5, NULL, 'code');
 
 -- --------------------------------------------------------
 
 INSERT INTO `users` (`id`, `school_id`, `username`, `email`, `password`, `created_on`, `active_end`, `ip_address`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `last_login`, `active`) VALUES
-    (1, 1, 'Albus Dumbledore', 'admin@admin.com', '$2y$10$hzsJ2B.y2LSu42yzB3gBMeyPIwUldqu/gefyn9gmLK570d9MBpyHm', '2000-01-01', '2100-01-01', '127.0.0.1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1577178686, 1),
-    (2, 1, 'Minerva McGalagony', 'admin2@admin.com', '$2y$08$200Z6ZZbp3RAEXoaWcMA6uJOFicwNZaqk4oDhqTUiFXFe63MG.Daa', '2000-01-01', '2010-01-01', '127.0.0.1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
+    (1, 1, 'Albus Dumbledore', 'admin@admin.com', '$2y$10$hzsJ2B.y2LSu42yzB3gBMeyPIwUldqu/gefyn9gmLK570d9MBpyHm', '2000-01-01', '2100-01-01', '127.0.0.1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1),
+    (2, 1, 'Minerva McGalagony', 'admin2@admin.com', '$2y$10$hzsJ2B.y2LSu42yzB3gBMeyPIwUldqu/gefyn9gmLK570d9MBpyHm', '2000-01-01', '2010-01-01', '127.0.0.1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1),
+    (3, 1, 'Test User 3', 'admin3@admin.com', '$2y$10$hzsJ2B.y2LSu42yzB3gBMeyPIwUldqu/gefyn9gmLK570d9MBpyHm', '2000-01-01', '2100-01-01', '127.0.0.1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
